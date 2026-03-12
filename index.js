13d58e6ed24fb2035db32b61b49e97d61cc29d99
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-03-12 18:11:51 +0000

    UPDATE: Small comment tweaks and workflow update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  clean-quotes - ✍️ Normalize quotes, dashes, and ellipsis in strings.
- *  @version: v1.0.2
+ *  @version: v1.0.3
  *  @link: https://github.com/tutyamxx/clean-quotes
  *  @license: MIT
  **/
@@ -23,8 +23,8 @@
 const cleanQuotes = (str) => (str ?? '')
     ?.replace(/[\u201C\u201D\u00AB\u00BB\u201E\u201F\u2033\u301D\u301E\u301F]/g, '"')       // --| Normalize double quotes
     ?.replace(/[\u2018\u2019\u201A\u201B\u2032\u2035\u02BC\u02BB\u02BD\uFF07]/g, "'")       // --| Normalize single quotes/apostrophes
-    ?.replace(/[\u2026\u22EF\u22F1\uFE45]/g, '...')                                         // --| Normalize dashes/hyphens
-    ?.replace(/[\u2013\u2014\u2212\u2015]/g, '-')                                           // --| Normalize ellipsis
+    ?.replace(/[\u2026\u22EF\u22F1\uFE45]/g, '...')                                         // --| Normalize ellipsis
+    ?.replace(/[\u2013\u2014\u2212\u2015]/g, '-')                                           // --| Normalize dashes/hyphens
     ?.replace(/\s+/g, ' ')                                                                  // --| Collapse multiple spaces into one and trim
     ?.trim();
 
